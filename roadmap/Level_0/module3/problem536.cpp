cff1333dd7925908b37a4d8b36ce7f6f23991ccc
Duong-1234 <cuns013@gmail.com>
2026-05-31 21:39:55 +0700

    solve problem 536

---
+++ roadmap/Level_0/module3/problem536.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int &i : a) cin>>i;
+    cout<<*min_element(a.begin(), a.end());
+    return 0;
+}
