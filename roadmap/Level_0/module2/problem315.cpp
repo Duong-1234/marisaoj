36f45090f7b8a5036627769b4c6f95232cc67e5e
Duong-1234 <cuns013@gmail.com>
2026-05-31 14:31:30 +0700

    solve problem 315"

--- roadmap/Level_0/module2/problem315.cpp
+++ roadmap/Level_0/module2/problem315.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int main() {
-    
+    int a, b;
+    cin>>a>>b;
+    int _gcd = __gcd(a,b);
+    cout<<a/_gcd<<" "<<b/_gcd;
+    return 0;
 }
