dd3015a4c40d777f406cb5ca270890232dffe478
Duong-1234 <cuns013@gmail.com>
2026-05-31 14:24:36 +0700

    solve problem 402

--- roadmap/Level_0/module2/problem402.cpp
+++ roadmap/Level_0/module2/problem402.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-    
+    long long n;
+    cin>>n;
+    long long result = 0, i = 1;
+    while(i < n) {
+        result++;
+        i *= 2;
+    }
+    cout<<result;
 }
