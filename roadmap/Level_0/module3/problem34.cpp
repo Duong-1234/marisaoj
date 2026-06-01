66ca03049d1f84fadff53694e2b2f2e7b06c95d2
Duong-1234 <cuns013@gmail.com>
2026-06-01 17:43:32 +0700

    solve problem 34

---
+++ roadmap/Level_0/module3/problem34.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int &i : a) cin>>i;
+    sort(a.begin(),a.end());
+    for(int i : a) cout<<i<<" ";
+    return 0;
+}
