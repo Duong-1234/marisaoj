5281145071e910ba2c9bce9b79f746f921edf7d5
Duong-1234 <cuns013@gmail.com>
2026-06-08 03:02:25 +0700

    module 4 complete

--- roadmap/Level_0/module4/problem413.cpp
+++ roadmap/Level_0/module4/problem413.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <sstream>
+#include <vector>
 using namespace std;
 int main() {
     string s;
     getline(cin, s);
     stringstream ss(s);
     string str;
+    vector<string> result;
     while (ss>>str) {  
         str[0] = toupper(str[0]);
         for(int i = 1 ; i < str.length() ; i++) 
             str[i] = tolower(str[i]);
-        cout<<str<<" ";
+        //cout<<str<<" ";
+        result.push_back(str);
     }
+    for(int i = 0 ; i < result.size() - 1 ; i++)
+        cout<<result[i]<<" ";
+    cout<<result[result.size()-1];
     return 0;
 }
