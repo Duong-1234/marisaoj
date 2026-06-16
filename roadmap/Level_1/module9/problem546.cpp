ca61bb0e7621fd25444c8327a0008d4b9ee8afd5
Duong-1234 <cuns013@gmail.com>
2026-06-16 16:22:04 +0700

    solve problem 546

---
+++ roadmap/Level_1/module9/problem546.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int n, k,a[21], x[21];
+void Try(int i) {
+    for(int j = 1 ; j <= n ; j++) {
+        if(a[j]) {
+            a[j] = 0;
+            x[i] = j;
+            if(i == k) {
+                for(int c = 1 ; c <= k ; c++)
+                cout<<x[c]<<" ";
+                cout<<'\n';
+            } else Try(i + 1);
+            a[j] = 1;
+        }
+    }
+}
+int main() {
+    cin>>n>>k;
+    fill(a, a + 21, 1);
+    Try(1);
+    return 0;
+}
