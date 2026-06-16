c21138ef29c44c84a8514dc2739f7851bb623d49
Duong-1234 <cuns013@gmail.com>
2026-06-16 17:49:49 +0700

    solve problem 59

---
+++ roadmap/Level_1/module10/problem59.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#define int long long
+signed main() {
+    int n, q;
+    cin>>n>>q;
+    int a[n + 1],pref[n + 1] = {0};
+    cin>>a[1];
+    pref[1] = a[1];
+    for(int i = 2 ; i <= n ; i++) {
+        cin>>a[i];
+        pref[i] = pref[i - 1] + a[i];
+    } 
+    while (q--) {
+        int l, r;
+        cin>>l>>r;
+        cout<<pref[r] - pref[l - 1]<<"\n";
+    }
+    
+    return 0;
+}
